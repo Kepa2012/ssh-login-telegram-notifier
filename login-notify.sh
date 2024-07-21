584bed659ef3a879af3627edc4b525205487f89f
Kepa2012 <kepchan2k12@yandex.by>
2024-07-21 10:01:56 +0300

    Update login-notify.sh

--- login-notify.sh
+++ login-notify.sh
@@ -6,7 +6,7 @@ login_date="$(date +"%e %b %Y, %a %r")"
 login_name="$(whoami)"
 server_name="$(hostname)"
 # For new line I use $'\n' here
-message="New login to server $server_name"$'\n'"$login_name"$'\n'"$login_ip"$'\n'"$login_date"
+message="💡 SSH login to server $server_name"$'\n'"🦸 $login_name"$'\n'"🌐 $login_ip"$'\n'"📅 $login_date"
 
 #send it to telegram
 telegram-send "$message"
